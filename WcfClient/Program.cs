7879835beed434cde2870feef5a8699de38eb336
Sumit Kadu (sumitkadu) <sumit.kadu@live.com>
2017-04-15 01:01:47 +0530

    First Commit

---
+++ WcfClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WcfService.RestServiceClient client = new WcfService.RestServiceClient();
+            //string content = client.GetData();
+
+            RemoteWcfHost.RestServiceClient client = new RemoteWcfHost.RestServiceClient();
+            string content = client.GetData();
+
+            Console.Write(content);
+            Console.ReadLine();
+        }
+    }
+}
